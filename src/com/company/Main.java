ca22ed332809a0677e62859f742b3c7ecb2faf16
emineakbulut <ezz.akbulut@gmail.com>
2021-01-28 14:45:56 -0800

    Sun added with errors

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,5 +8,10 @@ public class Main {
         FactorX.Sun=1;
         FactorX.Moon=3;
         FactorX.Planet=2;
+
+        SUN Freon=new SUN();
+        Freon.color="red";
+        Freon.size="7000km";
+        Freon.temp="40000c";
     }
 }
---
+++ src/com/company/SUN.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class SUN {
+
+    String size;
+    int temp;
+    String color;
+}
