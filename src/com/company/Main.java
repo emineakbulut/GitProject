195f5be2113c4cf407b02c918328bbe57fb4c5c3
emineakbulut <ezz.akbulut@gmail.com>
2021-01-28 14:34:56 -0800

    Solar System Deployed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        SolarSystem FactorX=new SolarSystem();
+        FactorX.Sun=1;
+        FactorX.Moon=3;
+        FactorX.Planet=2;
     }
 }
